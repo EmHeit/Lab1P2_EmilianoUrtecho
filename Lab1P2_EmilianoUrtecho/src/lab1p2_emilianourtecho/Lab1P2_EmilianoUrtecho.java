d334138f478ae21b523ba9c7f1672786139c985a
Lab1P1_EmilianoUrtecho <emilio.urtechon@gmail.com>
2023-10-13 19:21:59 -0600

    4 ss

--- Lab1P2_EmilianoUrtecho/src/lab1p2_emilianourtecho/Lab1P2_EmilianoUrtecho.java
+++ Lab1P2_EmilianoUrtecho/src/lab1p2_emilianourtecho/Lab1P2_EmilianoUrtecho.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
@@ -34,7 +35,6 @@ public class Lab1P2_EmilianoUrtecho {
                 case 1:// Ordenamiento de fechas
                     
                     CantidadFechas();
-                    OrdenarFechas();
                     
                     break;
                 case 2:// Registro Electronico
@@ -84,13 +84,13 @@ public class Lab1P2_EmilianoUrtecho {
             
             
 
-                System.out.print("{ ");
+                System.out.print("{");
 
  
             
             System.out.print(df2.format(Fecha));
  
-                System.out.print(" }");                
+                System.out.print("}");                
              
         }
         
@@ -102,35 +102,24 @@ public class Lab1P2_EmilianoUrtecho {
             
         }
         
+        StringTokenizer st = new StringTokenizer(fechasString, "/");
+        ArrayList<String> fechasToken = new ArrayList();
+        int numTokens = st.countTokens();
         
-        
-    }
-    
-    public static void OrdenarFechas(){
-
-        DateFormat df2 = new SimpleDateFormat("dd/MM/yyyy");
-
-        int n = fechas.size();
-        boolean cambio;
-        
-        do {
-            cambio = false;
-            for (int i = 1; i < n; i++) {
-                if (fechas.get(i-1).compareTo(fechas.get(i)) > 0) {
-                    Date temporal = fechas.get(i -1);
-                    fechas.set(i-1, fechas.get(i-1));
-                    fechas.set(i, temporal);
-                    cambio = true;
-                }
-            }
-            n--;
-        } while (cambio);
-        System.out.println("");
-        System.out.println("Ordenado: ");
-        for (int i = 0; i < fechas.size(); i++) {
-            Date fecha = fechas.get(i);
-            System.out.println(df2.format(fecha));
+        for (int i = 0; i < numTokens; i++) {
+            fechasToken.add(st.nextToken());
         }
+        
+        Collections.sort(fechasToken);
+        System.out.println("");
+        for (int i = 1; i < cantFechas; i++) {
+            
+            System.out.println("Iteracion "+i);
+            int numFechas = fechasToken.size();
+            for (int j = 0; j < numFechas; j++) {
+                System.out.print(fechasToken.get(i)+"/");
+            }
+        }
+        
     }
-    
 }
